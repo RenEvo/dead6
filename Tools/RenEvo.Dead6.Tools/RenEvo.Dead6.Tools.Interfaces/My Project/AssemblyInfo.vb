5d19fefe695d1ea544f18051410152f13379668b
Tom Anderson <dante.renevo@gmail.com>
2007-10-24 18:11:43 +0000

    Added new interface class, this will be used for any plugins to implement for the Application.Designer.vb
    Updated the ignores on some internal files such as the .suo, .user, and obj directories.

---
+++ Tools/RenEvo.Dead6.Tools/RenEvo.Dead6.Tools.Interfaces/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RenEvo.Dead6.Tools.Interfaces")> 
+<Assembly: AssemblyDescription("The Interfaces for the Dead Six Tools Application")> 
+<Assembly: AssemblyCompany("RenEvo Software & Designs")> 
+<Assembly: AssemblyProduct("The Dead Six Tools")> 
+<Assembly: AssemblyCopyright("Copyright © RenEvo Software & Designs 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("74d03f53-d19b-41a7-b995-ada611ccdf9c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
